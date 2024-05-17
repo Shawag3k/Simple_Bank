a0965582746b99ded40921aadc41b7ad345822b9
ShawaG3k <barbosa1235465@gmail.com>
2024-05-17 15:14:52 -0300

    adição da API e do metodo POST

---
+++ api/server.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(addres string) error {
+	return server.router.Run(addres)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
